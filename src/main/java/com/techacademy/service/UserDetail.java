80a34a1cfa137837e0dbe4a33aedbfb3e895bb0e
sayando <andousayuka@gmail.com>
2023-06-06 19:27:21 +0900

    L36 7.6

--- src/main/java/com/techacademy/service/UserDetail.java
+++ src/main/java/com/techacademy/service/UserDetail.java
@@ -2,8 +2,10 @@ package com.techacademy.service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.techacademy.entity.Employee;
@@ -12,11 +14,15 @@ public class UserDetail implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     private final Employee employee;
-    private final Collection<? extends GrantedAuthority> authorities;
+    private final List<SimpleGrantedAuthority> authorities;
+
+    public String name;
 
     public UserDetail(Employee employee) {
         this.employee = employee;
-        this.authorities = new ArrayList<GrantedAuthority>();
+        List<SimpleGrantedAuthority> simpleGrantedAuthoritis = new ArrayList<SimpleGrantedAuthority>();
+        simpleGrantedAuthoritis.add(new SimpleGrantedAuthority(employee.getAuthentication().getRole().toString()));
+        this.authorities = simpleGrantedAuthoritis;
     }
 
     public Employee getEmployee() {
@@ -40,25 +46,21 @@ public class UserDetail implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        // ユーザーが期限切れでなければtrueを返す
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // ユーザーがロックされていなければtrueを返す
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // ユーザーのパスワードが期限切れでなければtrueを返す
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // ユーザーが有効であればtrueを返す
         return true;
     }
 }
